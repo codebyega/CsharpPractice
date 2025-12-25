277dba2e73ba1d2aa5a403c0cb5d41c16919cf9f
codebyega <codebyega@outlook.com>
2025-12-25 17:41:42 +0700

    initial commit

---
+++ basics/generic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            storage<int> StorageInt = new storage<int>();
+            storage<string> StorageString = new storage<string>();
+
+            StorageInt.mengisi(10);
+            StorageString.mengisi("Hello broski");
+        }
+    class storage<T>
+        {
+            public T Value;
+                
+            public void mengisi(T value)
+            {
+                Value = value;
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
